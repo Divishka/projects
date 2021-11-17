81e74ddb275b4a149b1057fbdf892253d4182057
Divishka <danil7887@gmail.com>
2021-11-17 11:43:57 +0300

    ready

--- Inception/M/srcs/requirements/wordpress/conf/wordpress.sh
+++ Inception/M/srcs/requirements/wordpress/conf/wordpress.sh
@@ -10,9 +10,9 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	wp core install --allow-root \
 					--url=${WORDPRESS_WEBSITE_URL_WITHOUT_HTTP} \
 					--title=${WORDPRESS_WEBSITE_TITLE} \
-					--admin_user=${}
-					--admin_password=${}
-					--admin_email=${} \
+					--admin_user=${WORDPRESS_ADMIN_USER}
+					--admin_password=${WORDPRESS_ADMIN_PASSWORD}
+					--admin_email=${WORDPRESS_ADMIN_EMAIL} \
 					--path=/var/www/html/
 	wp user create --allow-root ${USER_NAME} ${USER_EMAIL} \
 					--user_pass=${USER_PASS} \
--- Inception/M/srcs/requirements/wordpress/conf/wp-config.php
+++ Inception/M/srcs/requirements/wordpress/conf/wp-config.php
@@ -1,10 +1,12 @@
 <?php
+
 define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'awhana' );
 define( 'DB_PASSWORD', 'proverka2718281');
 define( 'DB_HOST', 'mariadb' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
+
 define( 'AUTH_KEY',         'RANDOMCYPTKEY' );
 define( 'SECURE_AUTH_KEY',  'RANDOMCYPTKEY' );
 define( 'LOGGED_IN_KEY',    'RANDOMCYPTKEY' );
@@ -13,7 +15,9 @@ define( 'AUTH_SALT',        'RANDOMCYPTKEY' );
 define( 'SECURE_AUTH_SALT', 'RANDOMCYPTKEY' );
 define( 'LOGGED_IN_SALT',   'RANDOMCYPTKEY' );
 define( 'NONCE_SALT',       'RANDOMCYPTKEY' );
+
 $table_prefix = 'wp_';
+
 define( 'WP_DEBUG', false );
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -21,3 +25,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once ABSPATH . 'wp-settings.php';
+?>
